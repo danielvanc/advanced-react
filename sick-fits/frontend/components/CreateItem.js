794038c3b3f14f90253dfc12be9897685586b8d7
danielvanc <email@danielvanc>
2019-03-09 14:35:19 +0000

    Handle the uploading of images to Cloudinary.com

--- sick-fits/frontend/components/CreateItem.js
+++ sick-fits/frontend/components/CreateItem.js
@@ -41,6 +41,23 @@ class CreateItem extends Component {
       [name]: val, 
     })
   }
+   uploadFile = async (e) => {
+    console.log('Uploading file...');
+    const files = e.target.files;
+    const data = new FormData();
+    data.append('file', files[0]);
+    data.append('upload_preset', 'sickfits');
+    const res = await fetch('https://api.cloudinary.com/v1_1/danielvanc/image/upload', {
+      method: 'POST',
+      body: data
+    });
+    const file = await res.json();
+    console.log(file);
+    this.setState({
+      image: file.secure_url,
+      largeImage: file.eager[0].secure_url
+    });
+  }
   render() {
     return (
       <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
@@ -57,6 +74,11 @@ class CreateItem extends Component {
           }}>
             <Error error={error} />
             <fieldset disabled={loading} aria-busy={loading}>
+              <label htmlFor="file">
+                Image
+              <input type="file" id="file" name="file" placeholder="Upload an image" required  onChange={this.uploadFile} />
+              {this.state.image && <img width="200" src={this.state.image} alt="Upload Preview" />}
+              </label>
               <label htmlFor="title">
                 Title
               <input type="text" id="title" name="title" placeholder="Title" required value={this.state.title} onChange={this.handleChange} />
