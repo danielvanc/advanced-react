a471203c9b1e1544879d0d294fce2bacf8f4363e
danielvanc <email@danielvanc>
2019-10-16 15:14:21 +0100

    Frontend Password Reset Flow

---
+++ sick-fits/frontend/pages/reset.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import Reset from '../components/Reset'
+
+
+const ResetPassword = props => (
+  <div>
+    <p>Reset your password {props.query.resetToken}</p>
+    <Reset resetToken={props.query.resetToken} />
+  </div>
+)
+
+export default ResetPassword;
