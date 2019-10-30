15a7001631ba1125b280e05bea9995cb79063bf6
danielvanc <email@danielvanc>
2019-10-30 21:33:44 +0000

    Testing Order Components

--- sick-fits/frontend/components/TakeMyMoney.js
+++ sick-fits/frontend/components/TakeMyMoney.js
@@ -30,8 +30,6 @@ function totalItems(cart) {
 class TakeMyMoney extends React.Component {
   onToken = async (res, createOrder) => {
     NProgress.start()
-    console.log('On Token called')
-    console.log(res.id);
     // manually call the mutation once we have the stripe token
     const order = await createOrder({
       variables: {
@@ -48,7 +46,9 @@ class TakeMyMoney extends React.Component {
   render() {
     return (
       <User>
-        {({ data: {me} }) => (
+        {({ data: {me}, loading }) => {
+          if(loading) return null
+        return (
           <Mutation mutation={CREATE_ORDER_MUTATION} refetchQueries={[{ query: CURRENT_USER_QUERY}]}>
             {(createOrder) => (
               <StripeCheckout
@@ -65,10 +65,11 @@ class TakeMyMoney extends React.Component {
               </StripeCheckout>
             )}
           </Mutation>
-        )}
+        )}}
       </User>
     )
   }
 }
 
 export default TakeMyMoney;
+export { CREATE_ORDER_MUTATION }
